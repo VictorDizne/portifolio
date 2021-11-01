5d96eacd7a6495bd3301b037e43b661ea947923d
Victor Diniz <victoralbertocostadiniz@gmail.com>
2021-11-01 20:31:17 -0300

    Cria a página principal e a página sobre mim, mas ainda não conecta as duas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Context from './Context/Context';
 import InitalPage from './Pages/InitalPage';
+import MainBR from './Pages/MainBR';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ InitalPage } />
+          <Route exact path="/PT-BR" component={ MainBR }/>
         </Switch>
       </BrowserRouter>
     </Context>
---
+++ src/Pages/AboutMe.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <div>
+      <h3>Sobre Mim:</h3>
+      <p>
+          Comecei a aprender programação logo cedo, no meu sexto ano do ensino fundamental, pra ser mais exato, onde me juntei a um grupo de colegas e juntos participamos 
+          das Olímpiadas de robótica do meu colégio, que consistiam em nós construirmos o hardware e o software de uma maquina que competiria com outras, e foi aí que surgiu 
+          minha paixão
+        </p>
+        <p>
+          Por bastante tempo participei das Olímpiadas e consegui até levar algumas medalhas para casa, mas ao chegar no ensino médio eu escolhi parar temporariamente de 
+          mexer com programação e robótica.
+
+          Quando eu completei o ensino médio comecei a fazer faculdade de engenharia mecatrônica na FIAP, onde aprendi muito sobre 
+          metodologias ágeis e como coordenar um time para trabalhar juntos em um mesmo projeto.
+
+          Ao mesmo tempo descobri que minha paixão não era a robótica, e sim a programação, e foi aí que eu decidi de vez ir atrás de aprender programação, que foi onde eu 
+          encontrei a <a href="https://www.betrybe.com/">Trybe</a>, e junto com eles tenho aprendido muito mais sobre o mundo da programação, tendo múltiplos projetos para 
+          comprovar e exemplificar o que eu sei fazer. Além disso, aprendi muitos valores de Soft Skills, como comunicação, escuta ativa e até como trabalhar melhor em grupo.
+        </p>
+    </div>
+  );
+};
+
+export default AboutMe;
